390da03f649c73ca2a3c80d0b21aca4215aefe54
BRI AGUILAR <briaguilar@bris-air.home>
2020-02-20 16:33:24 -0600

    Had to delete the entire project and start a new one. HTML and Config files are good to go and project has been added to Lightning App Builder in SF. Need to write JS

---
+++ force-app/main/default/lwc/assignment/assignment.js
@@ -0,0 +1,40 @@
+import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+/* eslint no-extra-semi: "error" */
+
+
+
+export default class AssignmentOne extends LightningElement {
+    // @track name;
+    // @track industry;
+    // @track sector;
+    // @track phone;
+
+    // nameInput(event) {
+    //     this.name = event.target.value;
+    // };
+
+    // industryInput(event) {
+    //     this.industry = event.target.value;
+    // };
+
+    // sectorInput(event) {
+    //     this.sector = event.target.value;
+    // };
+
+    // phoneInput(event) {
+    //     this.phone = event.target.value;
+    // };
+
+    // handleSubmit() {
+    //     //  console.log(this.name);
+    //     //  console.log(this.industry);
+    //     //  console.log(this.sector);
+    //     //  console.log(this.phone);
+
+    //     const newRow = '<tr><td>' + this.name + '</td><td>' + this.industry + '</td><td>' + this.sector + '</td><td>' + this.phone + '</td></tr>';
+    //     const topTable = this.template.querySelector('.newinputs');
+
+    //     topTable.innerHTML = newRow;
+    // };
+}

41430d9e332e31f0f175e3ae5b4a779ea82b2eb8
BRI AGUILAR <briaguilar@bris-air.home>
2020-03-03 13:45:27 -0600

    Reads/gets user values. Places in top table. Table is not growing dynamically.

--- force-app/main/default/lwc/assignment/assignment.js
+++ force-app/main/default/lwc/assignment/assignment.js
@@ -1,7 +1,6 @@
 import { LightningElement, track } from 'lwc';
 
 
-
 export default class AssignmentOne extends LightningElement {
     @track name = '';
     @track industry = '';
@@ -28,18 +27,25 @@ export default class AssignmentOne extends LightningElement {
 
 
 
+
     @track rowName;
     @track rowIndustry;
     @track rowSector;
     @track rowPhone;
-
+    @track rows;
+    @track index;
+   
+   
+    
 
     handleSubmit() {
+        // Renders user inputs into top table
         this.rowName = `${this.name}`;
         this.rowIndustry = `${this.industry}`;
         this.rowSector = `${this.sector}`;
         this.rowPhone = `${this.phone}`;
 
+        // Resets all of the forms on page
         const inputFields = this.template.querySelectorAll('lightning-input');
         if (inputFields) {
             inputFields.forEach(each => {
@@ -48,6 +54,13 @@ export default class AssignmentOne extends LightningElement {
         }
     }
 
-
+    rows = [
+        {
+            name: this.rowName,
+            industry: this.rowIndustry,
+            sector: this.rowSector,
+            phone: this.rowPhone
+        }
+    ]
 
 }
